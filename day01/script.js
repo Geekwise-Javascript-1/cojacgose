d98f960269daf89f1bc59aa39723de4a382b9e77
cojacgose <cojacgose@gmail.com>
2016-08-10 18:16:21 -0700

    first push

---
+++ day01/script.js
@@ -0,0 +1,21 @@
+var name = "Corey";
+
+var a = 5;
+var b = 6;
+var c = a + b;
+
+var e = true;
+var f = false;
+var g;
+
+var nothing = null;
+var something = undefined;
+
+var arr = ["corey", 28, false];
+
+var car = {
+  make: "VW",
+  model: "Jetta",
+  year: 2002
+};
+console.log(car.model);
